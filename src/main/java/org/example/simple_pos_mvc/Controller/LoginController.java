3f340d7d2a4320121b9dc3700d0993d2df67ba2b
Binu0714 <binuj714@gmail.com>
2025-06-01 22:14:14 +0530

    add project dependancies

--- src/main/java/org/example/simple_pos_mvc/Controller/LoginController.java
+++ src/main/java/org/example/simple_pos_mvc/Controller/LoginController.java
@@ -1,4 +1,49 @@
 package org.example.simple_pos_mvc.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Hyperlink forgotPasswordLink;
+
+    @FXML
+    private ProgressIndicator loadingIndicator;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private CheckBox rememberMeCheckbox;
+
+    @FXML
+    private Hyperlink signUpLink;
+
+    @FXML
+    private TextField usernameField; 
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSignUp(ActionEvent event) {
+
+    }
+
 }
